b1e5d2822e11f7e0e920e92cfce64b27ad5ed166
kbj804 <rain1344@naver.com>
2020-03-04 14:33:03 +0900

    전처리 데이터 csv뽑고나서 question table이랑 answer table 삽입

--- test/korea_elect/insert_elect_data.py
+++ test/korea_elect/insert_elect_data.py
@@ -5,11 +5,14 @@ import psycopg2
 from sqlalchemy import create_engine
 import pandas as pd
 # import os
-# print(os.getcwd())
+#print(os.getcwd())
 
-insert_question_sql = "insert into qna_db.question (question, intent, sub_intent, q_morp, q_nn) values(%s, %s, %s, %s, %s)"
+insert_question_sql = "insert into qna_db.question (question, intent, sub_intent, q_morp, q_nn) values(%s, %s, %s, %s, %s) returning question_id"
+insert_answer_sql = "insert into qna_db.answer (answer, intent, question_id) values(%s, %s, %s)"
 
-def insert_question_table():
+# question Table에 질문데이터 삽입. 전처리 된 csv 파일이 필요함
+# question Table에 넣으면서 나오는 question_id를 이용하여 answer Table도 함께 넣음
+def insert_question_table(df):
     with connection.cursor() as curs:
         """
         0 - question
@@ -17,6 +20,9 @@ def insert_question_table():
         2 - sub_intent
         3 - q_morp
         4 - q_nn
+        5 - question_date
+        6 - answer_date
+        7 - answer
         """
         for i in range(len(df)):
             question = df.loc[i][0]
@@ -26,21 +32,29 @@ def insert_question_table():
             q_morp = df.loc[i][3]
             q_nn = df.loc[i][4]
 
+            answer = df.loc[i][7]
+
             curs.execute(insert_question_sql,(question, intent, sub_intent, q_morp, q_nn))
+            question_id = curs.fetchone() # (1054, ) 형태로 들어옴
+            insert_answer_table(answer, intent, question_id)
         connection.commit()
         
+def insert_answer_table(answer, intent, question_id):
+    with connection.cursor() as curs:
+        curs.execute(insert_answer_sql, (answer, intent, question_id))
+        
+
+
 
 # engine = create_engine('postgresql://postgres:1234@localhost:5432/postgres')
 # df.to_sql('qna_db.question', engine)
 
-print(len(df))
-print(len(df.columns))
 
 if __name__ == "__main__":
     connection = psycopg2.connect(host='localhost', user='postgres', password='1234', dbname='postgres')
 
-    df = pd.read_csv(r'./server_project/test/korea_elect/result/question_table.csv')
-    insert_question_table()
+    df = pd.read_csv(r'./server_project/test/korea_elect/result/korea_elect_preprocess.csv')
+    insert_question_table(df)
 
     connection.close()
 
--- test/korea_elect/korea_elect_preprocess.py
+++ test/korea_elect/korea_elect_preprocess.py
@@ -12,7 +12,7 @@ print(os.path.abspath(__file__))
 print(os.getcwd())
 
 
-data = pd.read_csv(r'./korea_elect/korea_elect_faq.csv')
+data = pd.read_csv(r'./korea_elect_faq.csv')
 data.head(2)
 
 # %% Columns 뭐 있나 확인
@@ -26,13 +26,13 @@ df
 df.rename(columns={'업무분류명':'intent', '등록일':'question_date', '수정일':'answer_date', '제목':'question', '내용':'answer', 'FAQ분류':'sub_intent', 'all_token':'q_morp', 'nn_token':'q_nn'}, inplace=True)
 df.head(5)
 # %%
-question_table = df[['question','intent','sub_intent','q_morp','q_nn',]]
-answer_table = df[['answer','intent',]]
+question_table = df[['question','intent','sub_intent','q_morp','q_nn','question_date','answer_date','answer']]
+#answer_table = df[['answer','intent',]]
 
 question_table
 # %%
-question_table.to_csv(r"./korea_elect/result/question_table.csv", index=False)
-answer_table.to_csv(r"./korea_elect/result/answer_table.csv", index=False)
+question_table.to_csv(r"./result/korea_elect_preprocess.csv", index=False)
+#answer_table.to_csv(r"./korea_elect/result/answer_table.csv", index=False)
 
 
 
